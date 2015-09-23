cce7e3783483d36dd385f9387fb2471101cbebd8
tylerbrown1857 <tyler.brown1857@myemail.indwes.edu>
2015-09-23 18:19:29 +0000

    done

--- PigLatin/PigLatin.py
+++ PigLatin/PigLatin.py
@@ -1,21 +1,26 @@
 __author__ = "Tyler Brown"
+__program__ = "PigLatinTranslator"
+__assignment__ = "WeekThreeLab"
 
 def main():
     x = 0
     v = False
     while x != 1:
-        word = input("Enter an english word: ")
+        word = input("Enter an english word to translate: ")
 #len(str)
         vowels = "aeiou"
         first = 'a'
+        #tests first letter consonant
         if word[0] in vowels :
             print ("This word starts with a vowel")
             v = True
         else :
             print ("This word starts with a consonant")
             v = False
+        #translates word if first is a vowel
         if v :
             word = word + "yay"
+        #translates word is first is a consonant
         else :
             first = word[0]
             word = word[1:len(word)]
